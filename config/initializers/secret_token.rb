9f27e0e6ac238470bdc9f73b5820651a047ddfe0
Anton Ignashev <ignashev@gmail.com>
2013-01-01 13:44:15 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bbss::Application.config.secret_token = 'cde3c3d2b885acfe1ca2cda885586e93a9fcc7b5e485c3ac30c5ca3bfcee69de428c84bd4eb67dfddf51d7ddb4800aeff4720b0190e44027f0400c4985d3d384'
